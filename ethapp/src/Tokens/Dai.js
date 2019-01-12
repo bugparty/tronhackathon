8594b495b1783653e7288e72e675299c66d74015
bowman <fancycode@gmail.com>
2019-01-12 09:32:22 +0800

    fix dai coin

--- ethapp/src/Tokens/Dai.js
+++ ethapp/src/Tokens/Dai.js
@@ -1,6 +1,6 @@
 import DaiToken from '../contractRes/DaiToken.json';
 export default {
-    address: "0xd5ee1e1ac5cf1ad4f99b189a1adda16e690f4f5f",
+    address: "0x74f7bee38064088096b4baf120cc9e9641b8c8a7",
     decimal: 5,
     name: "DaiToken",
     symbol: "Dai",
--- ethapp/src/Tokens/Pax.js
+++ ethapp/src/Tokens/Pax.js
@@ -1,6 +1,6 @@
 import PaxToken from '../contractRes/PaxToken.json';
 export default {
-    address: PaxToken.networks[5777].address,
+    address: "0x2b7a1b59ec05218d968be16a6f3408fb050d72ce",
     decimal: 5,
     name: "Paxos Standard Token",
     symbol: "Pax",
